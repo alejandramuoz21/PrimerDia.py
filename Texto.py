6a26b102f3b379c4552cb306594c35661ee86cd0
Guadalupe Muñoz <52288369+alejandramuoz21@users.noreply.github.com>
2019-07-02 22:11:53 -0500

    Add files via upload

---
+++ Texto.py
@@ -0,0 +1,48 @@
+print('Me llamo Lupita y tengo 20 años')
+
+
+print(10 - 2.5)
+
+
+print('10 por 3 son', 10*3)
+
+
+nombre= 'Almudena' #Guardo el nombre de la chica.
+edad= 19 #Guardo su edad.
+
+#Muestro su nombre:
+print('Te llamas', nombre)
+
+#A continuacion nuestr cuantos años tiene:
+print('Tienes', edad, 'años.')
+
+eded = edad + 1 #Le pongo un año mas.
+
+#Vuelvo a mostrar su edad:
+print('Cumples', edad, 'años ¡felicidades!')
+
+
+comida_niños = 50 #Tengo 50 bandejas de comida para niños.
+comida_adultos = 30 #Tengo 30 bandejas de comida para adultos.
+
+#Muestro las bandejas que tengo:
+print('Tienes', comida_niños, 'bandejas de coida para niños.')
+print('Tienes', comida_adultos, 'bandejas de comida para adultos.')
+
+#Simulo que los clientes han hecho consumiciones:
+comida_niños = comida_niños-40
+comida_adultos = comida_adultos-20
+
+#Sumo las bendajas que tengo y las guardo en otra variable:
+total_comida = comida_niños + comida_adultos
+
+print('Ha habido venta. En total quedan', total_comida, 'bandejas de comida.')
+
+
+name = str(input('Dame tu nombre'))
+age = int(input('Dame tu edad'))
+print("Te llamas", name, "tienes", age, "años")
+
+
+pi = float(input("Cual es el numero PI?"))
+print("El numero PI es", pi)
